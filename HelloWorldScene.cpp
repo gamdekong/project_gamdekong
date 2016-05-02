8c6102cf4d5a8b4cb18dd0f067f5f3986ee5ce2b
gamdekong <gamdekong@gmail.com>
2016-05-02 20:25:46 +0900

    첫등록

---
+++ HelloWorldScene.cpp
@@ -0,0 +1,28 @@
+﻿#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+   
+    auto scene = Scene::create();
+    auto layer = HelloWorld::create();
+	scene->addChild(layer);
+
+
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !LayerColor::initWithColor(Color4B(255, 255, 255, 255)) )
+    {
+        return false;
+    }
+   
+    return true;
+}
+
