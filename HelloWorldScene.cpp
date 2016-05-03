0dc9c82b7b6ffb88ab6a24265ddc20d943be0e1d
gamdekong <gamdekong@gmail.com>
2016-05-03 17:17:02 +0900

    5.3일 학원

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -23,51 +23,132 @@ bool HelloWorld::init()
         return false;
     }
 
-	
-
-	texture = Director::getInstance()->getTextureCache()->addImage("CloseNormal.png");
-	auto bg = Sprite::create("stage-1.png");
+	//----------------배경	
+	auto bg = Sprite::create("stage/stage-1.png");
 	bg->setAnchorPoint(Vec2(0, 0));
 	bg->setPosition(Vec2(0, 100));
-	this->addChild(bg);
+	//this->addChild(bg);
 
-	auto playerTexture = Director::getInstance()->getTextureCache()->addImage("move.png");
-	auto pSprite = Sprite::create("move.png", Rect(0, 0, 160, 160));
-	this->addChild(pSprite, 10);
-	pSprite->setPosition(Vec2(200, 200));
+	//----------------플레이어 텍스쳐
+	playerMoveTexture = Director::getInstance()->getTextureCache()->addImage("player/move/move.png");
+	playerAttackTexture = Director::getInstance()->getTextureCache()->addImage("player/attack/attack.png");
+	playerIdleTexture = Director::getInstance()->getTextureCache()->addImage("player/idle/idle.png");
+	playerDeadTexture = Director::getInstance()->getTextureCache()->addImage("player/dead/dead.png");
 
-	this->createWorld();
+	pSprite = Sprite::createWithTexture(playerIdleTexture, Rect(0, 0, 160, 160));
+	this->addChild(pSprite);
+	pSprite->setPosition(Vec2(300, 200));
 
-	auto animation = Animation::create();
-	animation->setDelayPerUnit(0.1);
+	
+	//--------이동	
+	auto animation1 = Animation::create();
+	animation1->setDelayPerUnit(0.1);
 	for (int i = 0; i < 6; i++)
 	{
 		int column = i % 6;
 
-		animation->addSpriteFrameWithTexture(playerTexture, Rect(160 * column, 0, 160, 160));
+		animation1->addSpriteFrameWithTexture(playerMoveTexture, Rect(160 * column, 0, 160, 160));
 	}
 
-	auto animate = Animate::create(animation);
+	auto animate1 = Animate::create(animation1);
 
-	auto seq = Sequence::create(animate, nullptr);
-	auto fSeq = RepeatForever::create(seq);
+	auto seq1 = Sequence::create(animate1, nullptr);
+	auto playerMoveSeq = RepeatForever::create(seq1);
 
-	pSprite->runAction(fSeq);
+	pSprite->runAction(playerMoveSeq);
 
+	//--------휴식
+	auto animation2 = Animation::create();
+	for (int i = 0; i < 4; i++)
+	{
+		int column = i % 4;
+
+		animation2->addSpriteFrameWithTexture(playerIdleTexture, Rect(160 * column, 0, 160, 160));
+	}
+
+	auto animate2 = Animate::create(animation2);
+
+	auto seq2 = Sequence::create(animate2, nullptr);
+	auto playerIdleSeq = RepeatForever::create(seq2);
+
+
+	//---------공격
+	auto animation3 = Animation::create();
+	for (int i = 0; i < 4; i++)
+	{
+		int column = i % 4;
+
+		animation3->addSpriteFrameWithTexture(playerAttackTexture, Rect(160 * column, 0, 160, 160));
+	}
+
+	auto animate3 = Animate::create(animation3);
+
+	auto seq3 = Sequence::create(animate3, nullptr);
+	auto playerAttackSeq = RepeatForever::create(seq3);
+
+	//----------죽음
+	auto animation4 = Animation::create();
+	for (int i = 0; i < 6; i++)
+	{
+		int column = i % 6;
+
+		animation4->addSpriteFrameWithTexture(playerDeadTexture, Rect(160 * column, 0, 160, 160));
+	}
+
+	auto animate4 = Animate::create(animation4);
+
+	auto seq4 = Sequence::create(animate4, nullptr);
+	auto playerDeadSeq = RepeatForever::create(seq4);
+
+
+
+	//-------------월드 생성
+	if (this->createWorld(true))
+		this->schedule(schedule_selector(HelloWorld::tick));
 
 	
+	
+	//--------------플레이어 바디생성
+	b2BodyDef playerBodyDef;
+	playerBodyDef.type = b2_dynamicBody;
+	playerBodyDef.position.Set(pSprite->getPosition().x / PTM_RATIO, pSprite->getPosition().y / PTM_RATIO);
+	playerBodyDef.linearDamping = 20;
+	playerBodyDef.userData = pSprite;
+	
+
+	playerBody = _world->CreateBody(&playerBodyDef);
+	
+	//playerBody->SetMassData(mass);
+	//playerBody->SetGravityScale(0);
+	
+	b2PolygonShape playerPolygon;
+	playerPolygon.SetAsBox((pSprite->getContentSize().width / 2) / PTM_RATIO, (pSprite->getContentSize().height / 2) / PTM_RATIO);
+
+	b2FixtureDef playerFixtureDef;
+	playerFixtureDef.shape = &playerPolygon;
+	playerFixtureDef.density = 0.0f;
+	
+	playerBody->CreateFixture(&playerFixtureDef);
 
 
+	//----조이스틱 생성
+	joystick1 = Joystick::create();
+	this->addChild(joystick1);
+	joyNum = 1;
+
+	joystick2 = Joystick::create();
+	this->addChild(joystick2);
+	
 
    
     return true;
 }
 
-void HelloWorld::createWorld()
+bool HelloWorld::createWorld(bool debug)
 {
 	//월드 생성 시작
 
-	b2Vec2 gravity = b2Vec2(0.0f, -30.0f);
+	b2Vec2 gravity = b2Vec2(0.0f, .0f);
 
 	//월드를 생성한다.
 	_world = new b2World(gravity);
@@ -77,6 +158,24 @@ void HelloWorld::createWorld()
 
 	//지속적인 물리작용을 할 것인지 결정한다. :테스트
 	_world->SetContinuousPhysics(true);
+
+
+	if (debug)
+	{
+		//적색 : 현재 물리 운동을 하는 것
+		//회색 : 현재 물리 운동량이 없는 것
+		m_debugDraw = new GLESDebugDraw(PTM_RATIO);
+		_world->SetDebugDraw(m_debugDraw);
+
+		uint32 flags = 0;
+
+		flags += b2Draw::e_shapeBit;
+
+		m_debugDraw->SetFlags(flags);
+	}
+
+
+
 
 	//가장자리(테두리)를 지정해 공간(Ground Box)을 만든다.
 
@@ -114,6 +213,8 @@ void HelloWorld::createWorld()
 
 	//월드 생성 끝-----------------------
 	this->schedule(schedule_selector(HelloWorld::tick));
+
+	return true;
 }
 
 
@@ -129,19 +230,26 @@ void HelloWorld::onEnter()
 {
 	Layer::onEnter();
 
-	//싱글 터치 모드로 터치리스너 등록
-	auto listener = EventListenerTouchOneByOne::create();
-	listener->setSwallowTouches(true);
-
-	listener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
-
-	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
 }
 
 void HelloWorld::onExit()
 {
 	_eventDispatcher->removeAllEventListeners();
 	Layer::onExit();
+}
+
+
+void HelloWorld::draw(Renderer * renderer, const Mat4 & transform, uint32_t flags)
+{
+	//Layer::draw();
+	Layer::draw(renderer, transform, flags);
+GL:ccGLEnableVertexAttribs(GL::VERTEX_ATTRIB_FLAG_POSITION);
+
+	kmGLPushMatrix();
+	_world->DrawDebugData();
+	kmGLPopMatrix();
+
+
 }
 
 void HelloWorld::tick(float dt)
@@ -160,7 +268,6 @@ void HelloWorld::tick(float dt)
 
 	//Step : 물리 세계를 시뮬레이션한다.
 	_world->Step(dt, velocityIterations, positionIterations);
-
 	//모든 물리 객체들은 링크드 리스트에 저장되어 참조해 볼수 있게 구현돼 있다.
 	//만들어진 객체만큼 루프를 돌리면서 바디에 붙인 스프라이트를 여기서 제어한다.
 	for (b2Body *b = _world->GetBodyList(); b; b = b->GetNext())
@@ -170,25 +277,32 @@ void HelloWorld::tick(float dt)
 			Sprite *spriteData = (Sprite*)b->GetUserData();
 			spriteData->setPosition(Vec2(b->GetPosition().x * PTM_RATIO, b->GetPosition().y * PTM_RATIO));
 			spriteData->setRotation(-1 * CC_RADIANS_TO_DEGREES(b->GetAngle()));
+
+			float vx = joystick1->getVelocity().x * 5;//스피드
+			float vy = joystick1->getVelocity().y * 5;
+			log("%f %f", vx, vy);
+			log("%f", b->GetLinearVelocity());
+			//set ball velocity by Joystick
+			b->SetLinearVelocity(b2Vec2(vx, vy) + (b->GetLinearVelocity()));
+			
 		}
+		if (!joystick1->getisPressed())
+			playerBody->SetLinearVelocity(b2Vec2(0, 0));
 	}
+	if (joystick1->getVelocity().x < 0)
+		pSprite->setFlippedX(true);
+	else
+		pSprite->setFlippedX(false);
+
 }
 
-bool HelloWorld::onTouchBegan(Touch *touch, Event *event)
-{
-	auto touchPoint = touch->getLocation();
-
-	//터치한 지점에 새로운 물리 객체의 바디와 해당 스프라이트를 추가한다.
-	addNewSpriteAtPosition(touchPoint);
-
-	return true;
-}
 
 void HelloWorld::addNewSpriteAtPosition(Vec2 location)
 {
 	//스프라이트를 파라미터로 넘어온 위치에 만든다.
 	Sprite *pSprite = Sprite::createWithTexture(texture, Rect(0, 0, 37, 37));
 	pSprite->setPosition(Vec2(location.x, location.y));
+	pSprite->setAnchorPoint(Vec2(0.5, 0.5));
 	this->addChild(pSprite);
 
 	//바디데프를 만들고 속성들을 지정한다.
@@ -209,7 +323,7 @@ void HelloWorld::addNewSpriteAtPosition(Vec2 location)
 	//바디에 적용할 물리 속성용 바디의 모양을 만든다.
 	//원형태를 선택해 반지름을 지정한다.
 	b2CircleShape circle;
-	circle.m_radius = 0.37f;
+	circle.m_radius = 0.3f;
 
 	//그리고 바디에 모양을(circle)을 고정시킨다.
 
--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -2,6 +2,13 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "Box2D/Box2D.h"
+#include "GLES-Render.h"
+#include "Joystick.h"
+
+#define PTM_RATIO 32
+
+using namespace cocos2d;
 
 class HelloWorld : public cocos2d::LayerColor
 {
@@ -12,6 +19,36 @@ public:
    
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+	~HelloWorld();
+	void onEnter();
+	void onExit();
+	void tick(float);
+	void draw(Renderer* renderer, const Mat4 &transform, uint32_t flags);
+
+	//bool onTouchBegan(Touch *touch, Event *event);
+	void addNewSpriteAtPosition(Vec2);
+	bool createWorld(bool);
+	
+	b2World *_world;
+	Joystick *joystick1;
+	Joystick *joystick2;
+	Sprite *pSprite;
+	b2Body *playerBody;
+	
+
+	/////Texture
+	Texture2D *texture;
+	Texture2D *playerMoveTexture;
+	Texture2D *playerAttackTexture;
+	Texture2D *playerIdleTexture;
+	Texture2D *playerDeadTexture;
+
+	
+
+
+	//for debugging
+	GLESDebugDraw *m_debugDraw;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
